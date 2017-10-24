662c1554a8b1ac6efd76ea12169e8750d61b1dbb
KuramotoRD <33013458+KuramotoRD@users.noreply.github.com>
2017-10-24 17:33:14 +0900

    JSONバリデーション追加

--- nodejs/routes/photoPut.js
+++ nodejs/routes/photoPut.js
@@ -5,6 +5,34 @@ var Cloudant = require('cloudant');
 var cloudant = Cloudant({instanceName: "Cloudant NoSQL DB-BeaconDataPool2", vcapServices: JSON.parse(process.env.VCAP_SERVICES)});
 var db = cloudant.db.use('beacondb');
 
+var Validator = require('jsonschema').Validator;
+var photoValidator = new Validator();
+
+// JSONスキーマ
+const photoSchema = {
+	"$schema": "http://json-schema.org/draft-04/schema#",
+	"type": "object",
+	"required": ["pid", "name", "type", "dataUrl"],
+	"properties": {
+		"pid": {
+			"description": "写真ID",
+			"type": "string"
+		},
+		"name": {
+			"description": "ファイル名",
+			"type": "string"
+		},
+		"type": {
+			"description": "拡張子",
+			"type": "string"
+		},
+		"dataUrl": {
+			"description": "接続URL",
+			"type": "string"
+		}
+	}
+};
+
 // 写真を追加するAPI
 router.get("/:photoId", function(req, res, next){
 	var photoData = {
@@ -27,6 +55,29 @@ router.get("/:photoId", function(req, res, next){
 router.post("/json", function(req, res, next){
 	console.log(req.body);
 
+	// JSONバリデーション
+	var check = photoValidator.validate(req.body, photoSchema);
+	if (check.errors.length > 0) {
+		console.log(check.errors);
+
+		var message = "";
+		for (i = 0; i < check.errors.length; i++){
+			if (i > 0) {
+				message += ", ";
+			}
+			var error = check.errors[i];
+			message += error.property + ":" + error.message;
+		}
+
+		res.json({
+			resultFlag: false,
+			message: message
+		});
+
+		return;
+	}
+
+	// データ登録
 	db.insert(req.body, function (err, result) {
 		if (err) {
 			console.log("DB Access Error!!!");
